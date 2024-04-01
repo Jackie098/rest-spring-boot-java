4d26c64674b373127da5fc0a5892be22268674d7
Carlos Augusto <carlos.aug.developer@gmail.com>
2024-04-01 11:45:25 -0300

    feat: added sub, mult, div and square functions

--- src/main/java/com/learn_java_spring/MathController.java
+++ src/main/java/com/learn_java_spring/MathController.java
@@ -19,6 +19,60 @@ public class MathController {
     return convertToDouble(numberOne) + convertToDouble(numberTwo);
   }
 
+  @GetMapping("/sub/{numberOne}/{numberTwo}")
+  public Double sub(
+      @PathVariable(value = "numberOne") String numberOne,
+      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+      throw new UnsupportedMathOperationException("Please set a numeric value");
+    }
+
+    return convertToDouble(numberOne) - convertToDouble(numberTwo);
+  }
+
+  @GetMapping("/mult/{numberOne}/{numberTwo}")
+  public Double mult(
+      @PathVariable(value = "numberOne") String numberOne,
+      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+      throw new UnsupportedMathOperationException("Please set a numeric value");
+    }
+
+    return convertToDouble(numberOne) * convertToDouble(numberTwo);
+  }
+
+  @GetMapping("/div/{numberOne}/{numberTwo}")
+  public Double div(
+      @PathVariable(value = "numberOne") String numberOne,
+      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+      throw new UnsupportedMathOperationException("Please set a numeric value");
+    }
+
+    return convertToDouble(numberOne) / convertToDouble(numberTwo);
+  }
+
+  @GetMapping("/avg/{numberOne}/{numberTwo}")
+  public Double avg(
+      @PathVariable(value = "numberOne") String numberOne,
+      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+      throw new UnsupportedMathOperationException("Please set a numeric value");
+    }
+
+    return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+  }
+
+  @GetMapping("/square/{number}")
+  public Double square(
+      @PathVariable(value = "number") String number) throws Exception {
+    if (!isNumeric(number)) {
+      throw new UnsupportedMathOperationException("Please set a numeric value");
+    }
+
+    return Math.sqrt(convertToDouble(number));
+  }
+
   private Double convertToDouble(String strNumber) {
     String number = strNumber.replaceAll(",", ".");
 
