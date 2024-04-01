d6e5056752b42348c4b9743d26e25ccba5bef7b2
Carlos Augusto <carlos.aug.developer@gmail.com>
2024-04-01 13:13:05 -0300

    feat: list all and search by person

--- src/main/java/com/learn_java_spring/controllers/PersonController.java
+++ src/main/java/com/learn_java_spring/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package com.learn_java_spring.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,14 @@ public class PersonController {
   @Autowired
   private PersonServices service;
 
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<Person> findAll() {
+
+    return service.findAll();
+  }
+
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Person findById(@PathVariable(value = "id") String id) throws Exception {
+  public Person findById(@PathVariable(value = "id") String id) {
 
     return service.findById(id);
   }
--- src/main/java/com/learn_java_spring/services/PersonServices.java
+++ src/main/java/com/learn_java_spring/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.learn_java_spring.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,19 @@ public class PersonServices {
   private final AtomicLong counter = new AtomicLong();
   private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+  public List<Person> findAll() {
+    logger.info("Finding all people");
+
+    List<Person> persons = new ArrayList<Person>();
+
+    for (int i = 0; i < 8; i++) {
+      Person person = mockPerson(i);
+      persons.add(person);
+    }
+
+    return persons;
+  }
+
   public Person findById(String id) {
     logger.info("Finding one person");
 
@@ -22,6 +37,18 @@ public class PersonServices {
     person.setLastName("Brand");
     person.setAddress("Rua Pa PA");
     person.setGender("Male");
+
+    return person;
+  }
+
+  private Person mockPerson(int i) {
+    Person person = new Person();
+
+    person.setId(counter.incrementAndGet());
+    person.setFirstName("Jack --" + i);
+    person.setLastName("Brand --" + i);
+    person.setAddress("Rua Pa PA --" + i);
+    person.setGender("Male -- " + i);
 
     return person;
   }
