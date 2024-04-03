3df68862e343932432cc5b70e2694f5dae45be8b
Carlos Augusto <carlos.aug.developer@gmail.com>
2024-04-03 10:17:24 -0300

    feat: added migrations to create Person table and insert some values

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+  (1, 'São Paulo', 'Airton', 'Male', 'Senna'),
+  (2, 'São Paulo', 'Trata', 'Male', 'HHH'),
+  (3, 'São Paulo', 'CACA', 'Female', 'AAA'),
